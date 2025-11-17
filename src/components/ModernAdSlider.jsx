8ad6e885d129b0f9fefa3929a7f08bc801338b5a
amowagroup-max <amowa.group@gmail.com>
2025-11-17 06:42:47 +0100

    Update ModernAdSlider.jsx

--- src/components/ModernAdSlider.jsx
+++ src/components/ModernAdSlider.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 
 export default function PureImageSlider() {
   const images = [
-    "https://scontent.falg7-2.fna.fbcdn.net/v/t39.30808-6/488600952_993754842880671_6088575747716362163_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=9m76BO1N9Q4Q7kNvwHgCQak&_nc_oc=AdnLPSW0B0rUf9dGa1RJMu_oIR_glfnjraMxY6FLAtDOqLXKVu-FBiSWH0pynCVX-K8&_nc_zt=23&_nc_ht=scontent.falg7-2.fna&_nc_gid=7_s8PS5qve7AeCFa5amhTw&oh=00_Afg1jz7j6jba4bbsPj-imlOrZ32gboLTTl-3iCR6VE4mOQ&oe=69175005",
+    "https://condor.dz/product-category/tv-image-et-son/gaming/",
     "https://scontent.falg6-1.fna.fbcdn.net/v/t39.30808-6/477039813_601456455988498_8827776879702323137_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=101&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=4T8S1nwo7JAQ7kNvwEzmVu3&_nc_oc=Adnor81iNR0J_Qh3HS1w-I0tjbhnF-56TJI4RzFno3RakZ0A4x7QCzplCdFdxvao6Gk&_nc_zt=23&_nc_ht=scontent.falg6-1.fna&_nc_gid=p22nrc16it9N_h7DUMQktA&oh=00_Afiulv5dv1rydsXFOXq5PCDeFzCbGZu0C7svlVYR-pNAZw&oe=691750BC",
   ];
 
